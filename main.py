d3f65e9808a36df126209dcdb19475edcb756f37
kamalikach <kamalika@gmail.com>
2026-02-16 15:55:05 +0000

    main sampling routine

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+import torch
+
+def sample_patches(model_a, model_b, data, k):
+    n = len(data)
+
+    rows = random.sample(range(n), k)
+    cols = random.sample(range(n), k)
+
+    return return_patches(model_a, model_b, data, rows, cols)
+
+def sample_square(model_a, model_b, data, k):
+    n = len(data)
+
+    idx = random.sample(range(n), k)
+    return return_patches(model_a, model_b, data, idx, idx)
+
+
+def return_patches(model_a, model_b, data, rows, cols):
+    emb_a_rows = [model_a(data[i]).flatten() for i in rows]
+    emb_a_cols = [model_a(data[j]).flatten() for j in cols]
+
+    emb_b_rows = [model_b(data[i]).flatten() for i in rows]
+    emb_b_cols = [model_b(data[j]).flatten() for j in cols]
+
+    a_list = [torch.dot(x, y) for x in emb_a_rows for y in emb_a_cols]
+    b_list = [torch.dot(x, y) for x in emb_b_rows for y in emb_b_cols]
+
+    return a_list, b_list
+
+
