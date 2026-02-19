03a8dc4e6466f1bb4cf16a65ee6a8e2b0aa35381
kamalikach <kamalika@gmail.com>
2026-02-19 01:44:33 +0000

    intermediate version

--- generate.py
+++ generate.py
@@ -1,17 +1,31 @@
-from generation.random import RandomGenerator
+from generation.adversarial_cifar import AdversarialCIFARGenerator
 from omegaconf import OmegaConf
+import timm
+import torch
 
-cfg_dict = { 'output_dir' : 'generated_datasets/random.pt', 
-        'N': 100, 
-        'C': 3,
-        'H': 224,
-        'W': 224
+cfg_dict = { 'output_dir' : 'generated_datasets/cifar_adv_eps_8.pt', 
+        'N': 100,
+        'epsilon': 8/255,
+        'target_H': 224,
+        'target_W': 224,
+        'device': 'cuda'
 }
+        # cfg.model = model used to generate adversarial examples
+        # cfg.cifar_loader = dataloader yielding (images, labels)
+        # cfg.target_H, cfg.target_W = output spatial size (e.g. 224, 224)
+        # cfg.output_dir = path where to save the data
+        # cfg.device = 'cuda' or 'cpu'
+
+
+
+
 
 def main(cfg):
-    cfg = OmegaConf.create(cfg_dict)
+    model = torch.hub.load("chenyaofo/pytorch-cifar-models", "cifar10_resnet20", pretrained=True)
 
-    generator = RandomGenerator(cfg)
+    cfg = OmegaConf.create(cfg_dict)
+    cfg.model = model
+    generator = AdversarialCIFARGenerator(cfg)
     ds = generator.generate()
     print(ds)
 
